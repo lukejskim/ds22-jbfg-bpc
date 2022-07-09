61b5048c8cbbfc42cc7b4dcccf48931113bc9f12
bigpy <lukejskim@gmail.com>
2022-07-10 03:52:19 +0900

    Bigpy Create : Practice Code

---
+++ notebook/s80-database-sql/database/common.py
@@ -0,0 +1,37 @@
+import pymysql
+
+# dbcon_param = {
+#     'host'    : 'localhost',
+#     'port'    : 3306,
+#     'user'    : 'root',
+#     'passwd'  : 'password',
+#     # 'db'      : 'bpcdb',
+#     'charset' : 'utf8',
+#     'autocommit' : True
+# }
+
+def get_connection(db_name='bpcdb'):
+    '''
+        DB Connection 객체를 생성한다.
+    '''
+    
+    dbcon_param = {
+        'host'    : 'localhost',
+        'port'    : 3306,
+        'user'    : 'root',
+        'passwd'  : 'password',
+        'db'      : db_name,
+        'charset' : 'utf8',
+        'autocommit' : True
+    }
+    
+    try:
+        # conn = pymysql.connect(db=db_name, **dbcon_param)
+        conn = pymysql.connect(**dbcon_param)
+    except Exception as err:
+        print('데이터베이스에 접속을 할 수 없습니다.')
+        print('DB Connection Error : {}'.format(err))
+
+    return conn
+
+
